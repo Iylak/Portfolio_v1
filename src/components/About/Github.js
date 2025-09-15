e35a8935bd729a337071472fb8042e1d06503bcb
Jie Wang <wang109@gmail.com>
2025-09-14 22:39:10 -0500

    Initial Revisions

--- src/components/About/Github.js
+++ src/components/About/Github.js
@@ -9,10 +9,10 @@ function Github() {
         Days I <strong className="purple">Code</strong>
       </h1>
       <GitHubCalendar
-        username="soumyajit4419"
+        username="Iylak"
         blockSize={15}
         blockMargin={5}
-        color="#c084f5"
+        color="#728FCE"
         fontSize={16}
       />
     </Row>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -23,7 +23,7 @@ function Footer() {
           <ul className="footer-icons">
             <li className="social-icons">
               <a
-                href="https://github.com/soumyajit4419"
+                href="https://github.com/Iylak"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
@@ -31,19 +31,19 @@ function Footer() {
                 <AiFillGithub />
               </a>
             </li>
+            {/*<li className="social-icons">*/}
+            {/*  <a*/}
+            {/*    href="https://twitter.com/Soumyajit4419"*/}
+            {/*    style={{ color: "white" }}*/}
+            {/*    target="_blank" */}
+            {/*    rel="noopener noreferrer"*/}
+            {/*  >*/}
+            {/*    <AiOutlineTwitter />*/}
+            {/*  </a>*/}
+            {/*</li>*/}
             <li className="social-icons">
               <a
-                href="https://twitter.com/Soumyajit4419"
-                style={{ color: "white" }}
-                target="_blank" 
-                rel="noopener noreferrer"
-              >
-                <AiOutlineTwitter />
-              </a>
-            </li>
-            <li className="social-icons">
-              <a
-                href="https://www.linkedin.com/in/soumyajit4419/"
+                href="https://www.linkedin.com/in/wangjeni/"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
@@ -51,16 +51,16 @@ function Footer() {
                 <FaLinkedinIn />
               </a>
             </li>
-            <li className="social-icons">
-              <a
-                href="https://www.instagram.com/soumyajit4419"
-                style={{ color: "white" }}
-                target="_blank" 
-                rel="noopener noreferrer"
-              >
-                <AiFillInstagram />
-              </a>
-            </li>
+            {/*<li className="social-icons">*/}
+            {/*  <a*/}
+            {/*    href="https://www.instagram.com/soumyajit4419"*/}
+            {/*    style={{ color: "white" }}*/}
+            {/*    target="_blank" */}
+            {/*    rel="noopener noreferrer"*/}
+            {/*  >*/}
+            {/*    <AiFillInstagram />*/}
+            {/*  </a>*/}
+            {/*</li>*/}
           </ul>
         </Col>
       </Row>
